7e53f9a3c61453511537ee6e8bb8cf4c4308c7d0
ghaoul <ghania.oularbi@ummto.dz>
2024-05-04 10:21:38 +0100

    commit chiffre

--- surface.js
+++ surface.js
@@ -1,8 +1,8 @@
 <script type="text/javascript" language="javascript">
-function CircleArea (r) {
+function calculsurface(r) {
    return 3.14 * (r * r);
 }
  
-document.write("l'aire d'un cercle de rayon de 25 cm est " +CircleArea(25) +"cm2<BR>");
+document.write("l'aire d'un cercle de rayon de 25 cm est " +Calculsurface(25) +"cm2<BR>");
 
 </script>
