c1bea252e4a7f97c36e4289f69028adfc1f15f7f
ghaoul <165478001+ghaoul@users.noreply.github.com>
2024-05-04 10:19:31 +0100

    Update surface.js

--- surface.js
+++ surface.js
@@ -1,6 +1,6 @@
 <script type="text/javascript" language="javascript">
 function CircleArea (r) {
-   return Math.PI * (r * r);
+   return 3.14 * (r * r);
 }
  
 document.write("l'aire d'un cercle de rayon de 25 cm est " +CircleArea(25) +"cm2<BR>");
